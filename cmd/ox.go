762b1ad6d5fd7a70771831badf39e169e3933fed
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2018-02-20 19:10:58 +0100

    implemented simple lookup to make sure the API keys work

---
+++ cmd/ox.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"ox"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s [word]")
+		os.Exit(1)
+	}
+	wordId := url.QueryEscape(os.Args[1])
+	searchURL := ox.APIBaseURL + "/entries/en/" + wordId
+	req, err := http.NewRequest("GET", searchURL, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "create request: %v\n", err)
+		os.Exit(1)
+	}
+	req.Header.Add("app_id", ox.ApplicationId)
+	req.Header.Add("app_key", ox.ApplicationKey)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "GET %s: %v\n", searchURL, err)
+		os.Exit(1)
+	}
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "GET %s: %s\n", searchURL, resp.Status)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+}
